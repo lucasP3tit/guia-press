5199098365836e21fa85034ca8efff0507e34296
Lucas Petit <lucas.a.petit@gmail.com>
2022-11-28 10:36:52 -0300

    criando models de categoria e artigo

---
+++ articles/Article.js
@@ -0,0 +1,19 @@
+const Sequelize = require("sequelize");
+const connection = require("../database/database");
+
+const article = connection.define("tb_articles",{
+    title:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    slug: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    body:{
+        type: Sequelize.TEXT,
+        allowNull: false
+    }
+});
+
+module.exports = article;
