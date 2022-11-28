f7302a6cfe915cc8c864c5c55b71f34e23341d0c
Lucas Petit <lucas.a.petit@gmail.com>
2022-11-28 19:21:34 -0300

    salvando e listando categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,12 +1,33 @@
 const express = require("express");
+const Category = require("./Category");
 const router = express.Router();
+const slugify = require("slugify");
 
- router.get("/categories", (req, res)=>{
-    res.send("Categoria");
+ router.get("/admin/categories", (req, res)=>{
+   Category.findAll()
+   .then(categories =>{
+      res.render("admin/categories/index", {categories: categories});
+   })
+   .catch(err => console.log("Error to get categories: ", err));
  });
 
  router.get("/admin/category/new", (req, res)=>{
-    res.send("Nova categoria")
+    res.render("admin/categories/new")
  })
 
+ router.post("/category/save", (req, res)=>{
+      let title = req.body.title;
+      if(title){
+         Category.create({
+            title: title,
+            slug: slugify(title)
+         }).then(()=>res.redirect("/"))
+      }else{
+         res.redirect("/admin/category/new");
+      }
+ });
+
+ router.get("/admin/categories", (req, res)=>{
+
+ })
  module.exports = router;
