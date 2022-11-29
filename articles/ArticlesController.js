a833db5fa7ac7fa58a7679d7d2af291cb17571ea
Lucas Petit <lucas.a.petit@gmail.com>
2022-11-29 13:44:38 -0300

    salvando e listando artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,13 +1,43 @@
 const express = require("express");
 const router = express.Router();
+const Article = require("./Article");
+const slugify = require("slugify");
+const Category = require("../categories/Category");
 
 
-router.get("/articles", (req, res)=>{
-    res.send("Articles router");
+router.get("/admin/articles", (req, res)=>{
+
+    Article.findAll({
+        include: [{
+            model: Category
+        }]
+    })
+    .then(articles=>{
+        res.render("admin/articles/index", {articles:articles})
+    });
+    
 });
 
 router.get("/admin/article/new", (req, res)=>{
-    res.render("admin/articles/new");
+    Category.findAll({order: [["title", "ASC"]]})
+    .then(categories =>{
+        res.render("admin/articles/new", {categories : categories});
+    })
+    .catch(err => console.log("An error occur when loading categories: ", err));
 });
+
+router.post("/admin/article/save", (req, res)=>{
+    let title = req.body.title;
+    let body = req.body.body;
+    let category = req.body.category;
+
+    Article.create({
+        title:title,
+        slug: slugify(title),
+        body: body,
+        tbCategoryId: category
+    })
+    .then(()=>res.redirect("/admin/articles"))
+})
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -6,7 +6,8 @@ const connection = new Sequelize(
     "1234567890",
     {
         host:"localhost",
-        dialect: "mysql"
+        dialect: "mysql",
+        timezone: "-03:00"
     }
 
 );
